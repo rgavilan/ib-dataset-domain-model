90d1f112d3dfcc2a15a89c45c97b4311498ce192
Luis Marcos Rivera <lmarcos@izertis.com>
2020-03-23 12:02:55 +0100

    SEMANTMURC-42 Paso 1: Creación de importador para todos los xml del primer dataset Personas

---
+++ src/main/java/es/um/asio/domain/personas/Persona.java
@@ -0,0 +1,13 @@
+package es.um.asio.domain.personas;
+
+import es.um.asio.domain.DataSetDataBase;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Persona extends DataSetDataBase {  
+    private long idPersona;
+}
