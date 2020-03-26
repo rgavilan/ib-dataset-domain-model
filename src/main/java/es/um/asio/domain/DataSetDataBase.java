ba76491ab3947bf77bf99487555ff2a573bef137
Luis Marcos Rivera <lmarcos@izertis.com>
2020-03-26 12:51:56 +0100

    SEMANTMURC-42 Paso 1: Creación de importador para todos los xml del primer dataset javadoc

--- src/main/java/es/um/asio/domain/DataSetDataBase.java
+++ src/main/java/es/um/asio/domain/DataSetDataBase.java
@@ -3,12 +3,15 @@ package es.um.asio.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * The class of representing generic data set data.
+ */
 @Getter
 @Setter
 public abstract class DataSetDataBase implements DataSetData {
     
-    /** 
-     * The version of DataSetData
-     * */
+    /**
+     * The version of DataSetData.
+     */
     private long version;
 }
