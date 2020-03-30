5ca0fbf3e06675ccdd38a23d43f608b3d4d6db75
Luis Marcos Rivera <lmarcos@izertis.com>
2020-03-30 14:59:52 +0200

    SEMANTMURC-42 Paso 1: Creación de importador para todos los xml del primer dataset Añadida nueva entidad EquipoProyecto y nuevo campo en RelacionOrigenProyecto

---
+++ src/main/java/es/um/asio/domain/proyectos/EquipoProyecto.java
@@ -0,0 +1,50 @@
+package es.um.asio.domain.proyectos;
+
+import javax.persistence.Entity;
+
+import es.um.asio.domain.DataSetDataBase;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Class that represents the mapping of the file "Equipos proyectos.xml"
+ */
+@Entity
+@Getter
+@Setter
+@ToString(includeFieldNames = true)
+@EqualsAndHashCode(callSuper = true)
+public class EquipoProyecto extends DataSetDataBase {
+    
+    /**
+     * Mapping of field IDPROYECTO.
+     */
+    private long idProyecto;
+    
+    /**
+     * Mapping of field NUMEROCOLABORADOR.
+     */
+    private long numeroColaborador;
+    
+    /**
+     * Mapping of field IDPERSONA.
+     */
+    private long idPersona;
+    
+    /**
+     * Mapping of field CODTITULACION.
+     */
+    private String codTitulacion;
+    
+    /**
+     * Mapping of field FECHAINICIO.
+     */
+    private String fechaInicio;
+    
+    /**
+     * Mapping of field FECHAFIN.
+     */
+    private String fechaFin;
+}
--- src/main/java/es/um/asio/domain/proyectos/RelacionOrigenProyecto.java
+++ src/main/java/es/um/asio/domain/proyectos/RelacionOrigenProyecto.java
@@ -1,6 +1,9 @@
 package es.um.asio.domain.proyectos;
 
+import javax.persistence.Entity;
+
 import es.um.asio.domain.DataSetDataBase;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,9 +11,11 @@ import lombok.ToString;
 /**
  * Class that represents the mapping of the file "Relaciones origenes proyectos.xml"
  */
+@Entity
 @Getter
 @Setter
-@ToString
+@ToString(includeFieldNames = true)
+@EqualsAndHashCode(callSuper = true)
 public class RelacionOrigenProyecto extends DataSetDataBase {  
     
     /**
@@ -22,4 +27,9 @@ public class RelacionOrigenProyecto extends DataSetDataBase {
      * Mapping of field CODTIPOORIGEN.
      */
     private String codTipoOrigen;
+    
+    /**
+     * Mapping of field IDORIGENPROYECTO.
+     */
+    private String idOrigenProyecto;
 }
