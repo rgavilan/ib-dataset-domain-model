77787a428efefd6164930ac4148938c3fc79a5da
hmartinez_izertis <hmartinez@izertis.com>
2020-09-16 17:28:18 +0200

    [SEMANTMURC-279] Fix

--- src/main/java/es/um/asio/domain/oaipmh/ListIdentifiersType.java
+++ src/main/java/es/um/asio/domain/oaipmh/ListIdentifiersType.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.Entity;
 
+import es.um.asio.domain.OperationableDataSetDataBase;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,7 +13,9 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString(includeFieldNames = true)
-public class ListIdentifiersType {
+public class ListIdentifiersType extends OperationableDataSetDataBase{
 
-    private List<HeaderType> header;
+	private static final long serialVersionUID = 3442274800948046070L;
+	
+	private List<HeaderType> header;
 }
