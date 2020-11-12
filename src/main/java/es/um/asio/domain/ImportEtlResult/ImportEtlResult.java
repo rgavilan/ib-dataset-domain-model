5d573a74562173a7a660a3a31f3c0f96085e0f7b
Atorrecilla <atorrecilla@izertis.com>
2020-11-12 17:20:17 +0100

    [SEMANTMURC-373] Concurrencia ETL

---
+++ src/main/java/es/um/asio/domain/ImportEtlResult/ImportEtlResult.java
@@ -0,0 +1,53 @@
+package es.um.asio.domain.ImportEtlResult;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import es.um.asio.domain.DataSetDataBase;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * The Class ImportEtlResult.
+ */
+@Entity
+@Getter
+@Setter
+@ToString(includeFieldNames = true)
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImportEtlResult extends DataSetDataBase{
+	/**
+	 * 
+	 */	
+	private static final long serialVersionUID = -8675168439808472063L;
+
+	/**
+	 * Date of import
+	 */
+	private Date dateTime; 
+	
+	/**
+	 * Status import
+	 */
+	private String status;
+	
+	/**
+	 * final message of import
+	 */
+	private String message;
+	
+	/**
+	 *  endpoint of import
+	 */
+	private String endpoint;
+	
+}
